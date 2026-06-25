b7104d0a0f0fd9147aded129d9858e3151a3019f
mohammadoh8 <mohammad_ho8@hotmail.com>
2026-06-25 23:58:51 +0300

    Init project: ASP.Net Core MVC with empty vehicles controller and view

---
+++ VehicleExplorer/VehicleExplorer/Controllers/VehiclesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleExplorer.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
